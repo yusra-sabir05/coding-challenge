7d22de074b173176adb26f6dd378b2508f4c5aef
yusra-sabie05 <yusrasabie185@gmail.com>
2024-04-29 23:54:23 +0500

    day 1

---
+++ 01-day/Qno-1/app.js
@@ -0,0 +1,4 @@
+// Personal Message: 
+// Store a person’s name in a variable and print a message to them, like “Hello yusra", would you like to learn some TypeScript today?”
+var myName = "yusra";
+console.log("Hello ".concat(myName, ", would you like to learn some typescript today?"));
---
+++ 01-day/Qno-1/app.ts
@@ -0,0 +1,7 @@
+// Personal Message: 
+// Store a person’s name in a variable and print a message to them, like “Hello yusra", would you like to learn some TypeScript today?”
+
+  let myName  = "yusra"
+
+  console.log(`Hello ${myName}, would you like to learn some typescript today?`)
+    
