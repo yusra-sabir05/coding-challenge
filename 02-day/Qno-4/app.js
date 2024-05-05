90854e14449c1abd7a0efbd12a58a6af58ae5485
yusra <161623126+yusra-sabir05@users.noreply.github.com>
2024-05-06 02:55:06 +0500

    Add files via upload

---
+++ 02-day/Qno-4/app.js
@@ -0,0 +1,4 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+var famous_person = "Toni Morrison";
+var message = ("".concat(famous_person, " said, If there's a book that you want to read, but it hasn't been written yet, then you must write it."));
+console.log(message);
---
+++ 02-day/Qno-4/app.ts
@@ -0,0 +1,8 @@
+// Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person. Then compose your message and store it in a new variable called message. Print your message.
+
+
+let famous_person = "Toni Morrison"
+
+let message = (`${famous_person} said, If there's a book that you want to read, but it hasn't been written yet, then you must write it.`)
+
+console.log(message);
